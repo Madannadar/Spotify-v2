1ca8d4ddc6bc4fd4c0e66db61299b26785b4c46e
Madannadar <madanak0509@gmail.com>
2025-06-03 14:14:23 +0530

    added some hard code data into the db and almost created the leftside bar

---
+++ Frontend/src/layout/components/LeftSidebar.tsx
@@ -0,0 +1,58 @@
+import PlaylistSkeleton from '@/components/skeletons/playlistSkeleton'
+import { buttonVariants } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { cn } from '@/lib/utils'
+import { SignedIn } from '@clerk/clerk-react'
+import { HomeIcon, Library } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+const LeftSidebar = () => {
+    const isLoading = true;
+    return (
+        <div className='h-full flex flex-col gap-2'>
+            <div className='rouded-lg bg-zinc-900 p-4'>
+                <div className='space-y-2'>
+                    <Link to={'/'} className={cn(buttonVariants(
+                        {
+                            variant: 'ghost',
+                            // size: 'icon',
+                            className: 'w-full justify-start text-zinc-400 hover:bg-zinc-800 hover:text-white'
+                        }
+                    ))}>
+                        <HomeIcon className='mr-2 size-5' />
+                        <span className='hidden md:inline'>Home</span>
+                    </Link>
+                    <SignedIn>
+                        <Link to={'/chat'} className={cn(buttonVariants(
+                            {
+                                variant: 'ghost',
+                                // size: 'icon',
+                                className: 'w-full justify-start text-zinc-400 hover:bg-zinc-800 hover:text-white'
+                            }
+                        ))}>
+                            <HomeIcon className='mr-2 size-5' />
+                            <span className='hidden md:inline'>Messages</span>
+                        </Link>
+                    </SignedIn>
+                </div>
+            </div>
+            <div className='flex-1 bg-zinc-900 p-4 rounded-lg'>
+                <div className='flex items-center justify-between mb-4'>
+                    <div className='flex items-center text-white px-2'>
+                        <Library className='size-5 mr-2'/>
+                        <span className='hidden md:inline'>Playlists</span>
+                    </div>
+                </div>
+                <ScrollArea className='h-[calc(100vh-300px)]'>
+                    <div className='space-y-2'>
+                        {isLoading ? (
+                            <PlaylistSkeleton />
+                        ):("some music playlists here")}
+                    </div>
+                </ScrollArea>
+            </div>
+        </div>
+    )
+}
+
+export default LeftSidebar
